255a1121f21bcf38b5a62eda0aebaa4dc6523ab3
tamelaos <tamelaos@student.21-school.ru>
2026-02-18 09:08:04 +0500

    beta

--- auth.py
+++ auth.py
@@ -1,6 +1,6 @@
 from dotenv import dotenv_values
 import hmac
 import hashlib
-config = dotenv_values("/run/secrets/auth/.env")
+config = dotenv_values("/run/secrets/common_python")
 def make_hmac(token: str) -> str:
     return hmac.new(config.get("SECRET_KEY", "").encode('utf-8'), token.encode('utf-8'), hashlib.sha256).hexdigest()
