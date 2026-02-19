9ad1a5eb52dcac29a6969047332ec40bb654ffde
KaraGOD <61622699+AlgoApi@users.noreply.github.com>
2026-02-19 18:15:22 +0500

    Update db_init.py

--- db_init.py
+++ db_init.py
@@ -1,11 +1,8 @@
 from sqlalchemy.ext.asyncio import create_async_engine, AsyncSession
 from sqlalchemy.orm import sessionmaker
-import os
-from dotenv import dotenv_values
-config = dotenv_values("/run/secrets/common_python")
+from os import getenv
 
-DATABASE_URL = config.get("DATABASE_URL", "")
-
+DATABASE_URL = getenv("DATABASE_URL")
 
 ENGINE = create_async_engine(DATABASE_URL, echo=False, future=True)
 AsyncSessionLocal = sessionmaker(bind=ENGINE, class_=AsyncSession, expire_on_commit=False)
